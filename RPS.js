941fbc7187418d2cbb49de36a7fad8b3292ef84e
Max <aghoprospermax@gmail.com>
2023-09-09 16:02:22 +0100

    RPS.js

--- RPS.js
+++ RPS.js
@@ -1,9 +1,22 @@
 const options = ["Rock" ,"paper", "scissors"];
+
 function getComputerChoice() {
     let choice = options[Math.floor(Math.random() * options.length)];
    return choice;
 }
 
 function getPlayerChoice() {
-    let validated = false 
+    let validatedInput = false;
+    while (validatedInput == false) {
+        const choice = prompt("Rock paper Scissors")
+        if (choice == null) {
+            continue;
+        }
+        const lowerChoice = choice.toLowerCase();
+        if (options.includes(lowerChoice)) {
+            validatedInput = true;
+            return lowerChoice;
+        }
+    }
+
 }
